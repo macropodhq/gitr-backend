0b030667247218398f21c1f6e405c00226e8b667
Alan Harper <alan@aussiegeek.net>
2014-12-22 15:48:59 +1100

    Return correct error if JWT expired

--- app/controllers/v1/application_controller.rb
+++ app/controllers/v1/application_controller.rb
@@ -7,5 +7,7 @@ class V1::ApplicationController < ::ApplicationController
     result = JWT.decode(token, ENV['JWT_SECRET_KEY'])
 
     @current_user == User.find(result[0]['user_id'])
+  rescue JWT::ExpiredSignature
+    render status: :unauthorized, json: {error: 'JWT expired'}
   end
 end
