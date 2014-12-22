8be1cdef76562fa2d3a544b1b32c7c73bc127436
Alan Harper <alan@aussiegeek.net>
2014-12-22 13:52:16 +1100

    Allow JWT auth using Authorization header

--- app/controllers/v1/application_controller.rb
+++ app/controllers/v1/application_controller.rb
@@ -2,7 +2,9 @@ class V1::ApplicationController < ::ApplicationController
   protect_from_forgery with: :null_session
 
   def login_required
-    result = JWT.decode(params[:token], ENV['JWT_SECRET_KEY'])
+    token = request.headers['Authorization'].split(' ').last
+    token = params[:token] if token.blank?
+    result = JWT.decode(token, ENV['JWT_SECRET_KEY'])
 
     @current_user == User.find(result[0]['user_id'])
   end
