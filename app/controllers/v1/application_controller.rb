5423b9ff233d2623f454aca0c46d868a88f38007
Alan Harper <alan@aussiegeek.net>
2014-12-22 16:04:30 +1100

    Handle requests correclty without auth header/param

--- app/controllers/v1/application_controller.rb
+++ app/controllers/v1/application_controller.rb
@@ -2,8 +2,13 @@ class V1::ApplicationController < ::ApplicationController
   protect_from_forgery with: :null_session
 
   def login_required
-    token = request.headers['Authorization'].split(' ').last
+    token = request.headers['Authorization'].split(' ').last rescue nil
     token = params[:token] if token.blank?
+
+    if token.blank?
+      render status: :unauthorized, json: {error: 'No JWT received'}
+      return
+    end
     result = JWT.decode(token, ENV['JWT_SECRET_KEY'])
 
     @current_user = User.find(result[0]['user_id'])
