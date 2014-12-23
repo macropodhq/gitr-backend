2d0a15f743b3c785ef5180647a744beb1d47b618
Alan Harper <alan@aussiegeek.net>
2014-12-23 13:55:09 +1100

    Make JWT last 24 hours

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   def generate_jwt
     JWT.encode({
                    user_id: id,
-                   exp: 1.hour.from_now.to_i
+                   exp: 1.day.from_now.to_i
                },
                ENV['JWT_SECRET_KEY']
     )
