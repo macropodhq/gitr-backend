d19f855c11164150713595171df125180a46b099
Alan Harper <alan@aussiegeek.net>
2014-12-24 08:14:39 +1100

    Show recently active users first

--- app/controllers/v1/people_controller.rb
+++ app/controllers/v1/people_controller.rb
@@ -1,6 +1,6 @@
 class V1::PeopleController < V1::ApplicationController
   def index
-    @users = User.limit(5).where.not(id: [current_user.id] + current_user.matches.map(&:other_user_id))
+    @users = User.limit(5).where.not(id: [current_user.id] + current_user.matches.map(&:other_user_id)).order('last_seen_at')
   end
 
   def show
