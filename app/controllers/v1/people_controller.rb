bbc3a99300177b5694f709dafe966cf1e8984410
Alan Harper <alan@aussiegeek.net>
2014-12-24 12:52:54 +1100

    Don't show users until we have fetched their profile

--- app/controllers/v1/people_controller.rb
+++ app/controllers/v1/people_controller.rb
@@ -1,6 +1,6 @@
 class V1::PeopleController < V1::ApplicationController
   def index
-    @users = User.limit(5).where.not(id: [current_user.id] + current_user.matches.map(&:other_user_id)).order('last_seen_at')
+    @users = User.limit(5).where.not(id: [current_user.id] + current_user.matches.map(&:other_user_id)).order('last_seen_at').where('avatar_url is not null')
   end
 
   def show
