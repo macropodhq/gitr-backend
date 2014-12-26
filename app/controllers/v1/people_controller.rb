e134afc42bfd038b851b3933ebe6c12c5518684e
Alan Harper <alan@aussiegeek.net>
2014-12-26 13:22:54 +1100

    Show users who matched you first

--- app/controllers/v1/people_controller.rb
+++ app/controllers/v1/people_controller.rb
@@ -1,6 +1,7 @@
 class V1::PeopleController < V1::ApplicationController
   def index
-    @users = User.limit(5).where.not(id: [current_user.id] + current_user.matches.map(&:other_user_id)).order('last_seen_at').where('avatar_url is not null')
+    @users = User.find_by_sql ["select users.* from matches inner join users on matches.user_id=users.id where other_user_id=:user_id and match='y' and user_id not in (select other_user_id from matches where user_id=:user_id);", user_id: current_user.id]
+    @users = User.limit(5).where.not(id: [current_user.id] + current_user.matches.map(&:other_user_id)).order('last_seen_at').where('avatar_url is not null') if @users.empty?
   end
 
   def show
